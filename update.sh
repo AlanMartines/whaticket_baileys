11140db8b1fb261391e8f557fa065b7e1add1e55
Alan Martines <alancpmartines@hotmail.com>
2024-01-07 07:55:19 -0400

    Update

--- update.sh
+++ update.sh
@@ -3,13 +3,15 @@ echo "Updating Whaticket, please wait."
 
 git pull
 cd backend
-npm install
 rm -rf dist
+rm -fr package-lock.json
+npm install
 npm run build
 npx sequelize db:migrate
 cd ../frontend
-npm install
 rm -rf build
+rm -fr package-lock.json
+npm install
 npm run build
 pm2 restart unkbot-backend && pm2 restart unkbot-frontend
 
