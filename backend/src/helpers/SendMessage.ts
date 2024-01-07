6276a42267594eaf2f14d7dfe59b7f3df35e53d9
Alan Martines <alancpmartines@hotmail.com>
2024-01-07 15:21:24 -0400

    Update

--- backend/src/helpers/SendMessage.ts
+++ backend/src/helpers/SendMessage.ts
@@ -5,69 +5,93 @@ import fs from "fs";
 import { getMessageOptions } from "../services/WbotServices/SendWhatsAppMedia";
 
 export type MessageData = {
-  number: number | string;
-  body: string;
-  mediaPath?: string;
+	number: number | string;
+	body: string;
+	mediaPath?: string;
 };
 
 export const SendMessage = async (
-  whatsapp: Whatsapp,
-  messageData: MessageData
+	whatsapp: Whatsapp,
+	messageData: MessageData
 ): Promise<any> => {
-  try {
-    const wbot = await GetWhatsappWbot(whatsapp);
-    const chatId = `${messageData.number}@s.whatsapp.net`;
+	try {
+		const wbot = await GetWhatsappWbot(whatsapp);
+		const chatId = `${messageData.number}@s.whatsapp.net`;
 
-    let message;
+		let message;
 
-    if (messageData.mediaPath) {
-      const options = await getMessageOptions(
-        messageData.body,
-        messageData.mediaPath
-      );
-      if (options) {
-        message = await wbot.sendMessage(chatId, {
-          ...options
-        });
-      }
-    } else {
-      const body = `\u200e${messageData.body}`;
-      message = await wbot.sendMessage(chatId, { text: body });
-    }
+		if (messageData.mediaPath) {
+			const options = await getMessageOptions(
+				messageData.body,
+				messageData.mediaPath
+			);
+			if (options) {
+				message = await wbot.sendMessage(chatId, {
+					...options
+				}).then(async (result: any) => {
+					//
+					return result;
+					//
+				}).catch(async (err: any) => {
+					throw new Error(err);
+				});
+			}
+		} else {
+			const body = `\u200e${messageData.body}`;
+			message = await wbot.sendMessage(chatId, { text: body }).then(async (result: any) => {
+				//
+				return result;
+				//
+			}).catch(async (err: any) => {
+				throw new Error(err);
+			});
+		}
 
-    return message;
-  } catch (err: any) {
-    throw new Error(err);
-  }
+		return message;
+	} catch (err: any) {
+		throw new Error(err);
+	}
 };
 
 export const SendMessageGroup = async (
-  whatsapp: Whatsapp,
-  messageData: MessageData
+	whatsapp: Whatsapp,
+	messageData: MessageData
 ): Promise<any> => {
-  try {
-    const wbot = await GetWhatsappWbot(whatsapp);
-    const chatId = `${messageData.number}@g.us`;
+	try {
+		const wbot = await GetWhatsappWbot(whatsapp);
+		const chatId = `${messageData.number}@g.us`;
 
-    let message;
+		let message;
 
-    if (messageData.mediaPath) {
-      const options = await getMessageOptions(
-        messageData.body,
-        messageData.mediaPath
-      );
-      if (options) {
-        message = await wbot.sendMessage(chatId, {
-          ...options
-        });
-      }
-    } else {
-      const body = `\u200e${messageData.body}`;
-      message = await wbot.sendMessage(chatId, { text: body });
-    }
+		if (messageData.mediaPath) {
+			const options = await getMessageOptions(
+				messageData.body,
+				messageData.mediaPath
+			);
+			if (options) {
+				message = await wbot.sendMessage(chatId, {
+					...options
+				}).then(async (result: any) => {
+					//
+					return result;
+					//
+				}).catch(async (err: any) => {
+					throw new Error(err);
+				});
+			}
+		} else {
+			const body = `\u200e${messageData.body}`;
+			message = await wbot.sendMessage(chatId, { text: body }).then(async (result: any) => {
+				//
+				return result;
+				//
+			}).catch(async (err: any) => {
+				throw new Error(err);
+			});
+		}
 
-    return message;
-  } catch (err: any) {
-    throw new Error(err);
-  }
+		return message;
+	} catch (err: any) {
+		throw new Error(err);
+	}
 };
